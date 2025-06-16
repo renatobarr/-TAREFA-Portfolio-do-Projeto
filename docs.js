ca8634ad3f51a28b1d3b28c485acd626969e2ed1
Renato Barreto <renato.barreto@edu.unifil.br>
2025-06-16 11:40:35 -0300

    atualizando documentos

--- docs.js
+++ docs.js
@@ -10,6 +10,26 @@ document.getElementById("quemSomos").addEventListener("click", function(){
     window.location.href = "quemSomos.html"
 });
 
-function openPDF() {
+function openPDF1() {
+    window.open("./docs/Documento de Visão - Projeto de Extensão.pdf", "_blank");
+  }
+
+  function openPDF2() {
+    window.open("./docs/MVP E CASO DE USO.pdf", "_blank");
+  }
+
+
+  function openPDF3() {
+     window.open("./docs/MVP E CASO DE USO.pdf", "_blank");
+  }
+
+
+  function openPDF4() {
     window.open("./docs/vision_informal.pdf", "_blank");
   }
+
+
+  function openPDF5() {
+    window.open("./docs/vision_informal.pdf", "_blank");
+  }
+
